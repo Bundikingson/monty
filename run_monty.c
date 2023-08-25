0fa6bdb3f2d97d2fcbd661a50e8c91dd882d7fa3
Bundikingson <Mwaikingson@gmail.com>
2023-08-25 05:53:24 -0700

    monty

--- run_monty.c
+++ run_monty.c
@@ -122,8 +122,14 @@ int run_monty(FILE *script_fd)
 {
     stack_t *stack = NULL;
     char *line = NULL;
-    size_t len = 0; /* Initialize len to a suitable value */
     size_t line_capacity = /* specify the buffer size */;
+    size_t exit_status = EXIT_SUCCESS;
+    unsigned int line_number = 0, prev_tok_len = 0;
+    void (*op_func)(stack_t**, unsigned int);
+
+    if (init_stack(&stack) == EXIT_FAILURE)
+        return (EXIT_FAILURE);
+
     line = (char *)malloc(line_capacity);
     if (line == NULL) {
         /* Handle memory allocation failure */
@@ -136,52 +142,22 @@ int run_monty(FILE *script_fd)
     if (init_stack(&stack) == EXIT_FAILURE)
         return (EXIT_FAILURE);
 
-    while (fgets(line, line_capacity, script_fd) != NULL)
-{
-line_number++;
-op_toks = strtow(line, DELIMS);
+ while (fgets(line, line_capacity, script_fd) != NULL)
+    {
+        line_number++;
+        op_toks = strtow(line, DELIMS);
 if (op_toks == NULL)
 {
 if (is_empty_line(line, DELIMS))
 continue;
-free_stack(&stack);
-return (malloc_error());
-}
-else if (op_toks[0][0] == '#') /* comment line */
-{
-free_tokens();
-continue;
-}
-op_func = get_op_func(op_toks[0]);
-if (op_func == NULL)
-{
-free_stack(&stack);
-exit_status = unknown_op_error(op_toks[0], line_number);
-free_tokens();
-break;
-}
-prev_tok_len = token_arr_len();
-op_func(&stack, line_number);
-if (token_arr_len() != prev_tok_len)
-{
-if (op_toks && op_toks[prev_tok_len])
-exit_status = atoi(op_toks[prev_tok_len]);
-else
-exit_status = EXIT_FAILURE;
-free_tokens();
-break;
-}
-free_tokens();
-}
-free_stack(&stack);
- 
-if (line && *line == 0)
-{
-free(line);
-return (malloc_error());
-}
- 
-free(line);
-return (exit_status);
-}
+ free_stack(&stack);
 
+    if (line && *line == 0)
+    {
+        free(line);
+        return (malloc_error());
+    }
+
+    free(line);
+    return (exit_status);
+}
